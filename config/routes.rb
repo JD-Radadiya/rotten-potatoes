0e14e05e486d56413b570dd787549b75723cdd2a
Jaydeep Radadiya <jdr@tamu.edu>
2023-09-13 21:28:32 -0500

    Updated Routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :movies do
-    get 'rating', 'release', on: :collection
-  end
+  resources :movies
   root :to => redirect('/movies')
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
