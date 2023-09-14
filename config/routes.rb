dbf979e4b3aaedd4dfae83ec52d8c4f7f4d97548
Jaydeep Radadiya <jdr@tamu.edu>
2023-09-13 19:50:31 -0500

    Trying to see what the error is about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :movies do
-    get 'rating', 'release', on: :collection
-  end
-  root :to => redirect('/movies')
-  # Defines the root path route ("/")
-  # root "articles#index"
+resources :movies do
+get 'rating', 'release', on: :collection
+end
+root :to => redirect('/movies')
 end
